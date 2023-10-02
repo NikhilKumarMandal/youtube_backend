8526af6b4530500241b37db3c08db824a5c57272
“[Nikhil <[nikhilkumarmandal946@gmail.com]>
2023-10-02 21:18:18 +0530

    index.js

--- chai-aur-react/07reactRouter/src/components/Footer/Footer.jsx
+++ chai-aur-react/07reactRouter/src/components/Footer/Footer.jsx
@@ -35,14 +35,14 @@ export default function Footer() {
                             <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase">Follow us</h2>
                             <ul className="text-gray-500 font-medium">
                                 <li className="mb-4">
-                                    <a
-                                        href="https://github.com/hiteshchoudhary"
+                                    <Link
+                                        to= '/github'
                                         className="hover:underline"
                                         target="_blank"
                                         rel="noreferrer"
                                     >
                                         Github
-                                    </a>
+                                    </Link>
                                 </li>
                                 <li>
                                     <Link to="/" className="hover:underline">
@@ -73,7 +73,7 @@ export default function Footer() {
                     <span className="text-sm text-gray-500 sm:text-center">
                         © 2023
                         <a href="https://hiteshchoudhary.com/" className="hover:underline">
-                            hiteshchoudhary
+                            NikhilKumar
                         </a>
                         . All Rights Reserved.
                     </span>
